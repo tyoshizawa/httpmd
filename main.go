24c60330e7f67276487a7b7cc9363151106bcd89
Takeshi Yoshizawa <zawatak@gmail.com>
2016-06-10 13:48:30 -0700

    starting from simple static file server

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.FileSystem(http.Dir("."))))
+	log.Println("Listening at 0.0.0.0:8888")
+	http.ListenAndServe("0.0.0.0:8888", mux)
 }
